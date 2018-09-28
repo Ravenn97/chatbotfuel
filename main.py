777fcaa447eae85ebea4a83e37f07887a5707f50
tungdv <tungdovan@admicro.vn>
2018-09-28 10:15:10 +0700

    avc

--- main.py
+++ main.py
@@ -24,12 +24,7 @@ def crawl_weather():
     soup = BS(data, "html.parser")
     result = soup.find("td",class_="ttCel").get_text().replace("\n"," ").strip()
     data = "Hôm nay nhiệt độ Hà Nội{}".format(result)
-    return  json.dumps({
- "messages": [
-   {"text": data},
-
- ]
-})
+    return  '{"messages": [{"text": {}}}]}'.format(data)
 
 @app.route("/place", methods=['GET', 'POST'])
 def crawl_tea():
